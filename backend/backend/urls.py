99ee901f3fc5a3c71e7c0300bebdcba5c2b2e6af
Vitor <vitorrrenk@gmail.com>
2025-06-17 19:00:25 -0300

    algumas mudanças no requirements.txt e urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,12 +3,12 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.views.generic.base import RedirectView
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
+    path('', RedirectView.as_view(url='/admin/', permanent=False)),
     path('',TemplateView.as_view(template_name='index.html')),
     path('api/products/',include('api.urls.product_urls')),
     path('api/users/',include('api.urls.user_urls')),
